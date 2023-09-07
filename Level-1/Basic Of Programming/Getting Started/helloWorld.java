0477b817defd540e532d40e1fef945ff05e7d421
Harsh Sangwan <84121947+harsh-sangwan2003@users.noreply.github.com>
2023-09-07 09:50:39 +0530

    first commit

---
+++ Level-1/Basic Of Programming/Getting Started/helloWorld.java
@@ -0,0 +1,8 @@
+public class helloWorld {
+
+    public static void main(String[] args) {
+
+        System.out.print("Hello World\n");
+        System.out.println("Hello World");
+    }
+}
---
+++ Level-1/Basic Of Programming/Getting Started/star.java
@@ -0,0 +1,7 @@
+public class star {
+
+    public static void main(String[] args) {
+
+        System.out.println("*\n**\n***\n****\n*****");
+    }
+}
