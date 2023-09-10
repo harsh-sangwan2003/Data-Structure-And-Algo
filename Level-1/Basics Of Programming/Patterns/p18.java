5ef839434c336aeb92a29854f88acab0090da3df
Harsh Sangwan <84121947+harsh-sangwan2003@users.noreply.github.com>
2023-09-10 09:24:57 +0530

    patterns

---
+++ Level-1/Basics Of Programming/Patterns/p18.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class p18 {
+
+    public static void main(String[] args) {
+
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+
+        int nst = 1, nsp = n / 2;
+
+        for (int r = 1; r <= n; r++) {
+
+            for (int csp = 1; csp <= nsp; csp++)
+                System.out.print("  ");
+
+            for (int cst = 1; cst <= nst; cst++)
+                System.out.print("*" + " ");
+
+            System.out.println();
+
+            if (r <= n / 2) {
+
+                nsp--;
+                nst += 2;
+            }
+
+            else {
+
+                nsp++;
+                nst -= 2;
+            }
+        }
+
+        scn.close();
+    }
+} 
