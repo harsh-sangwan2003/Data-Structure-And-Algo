f9365d50e13dd5ec609517f346b115f9124d1e15
Harsh Sangwan <84121947+harsh-sangwan2003@users.noreply.github.com>
2023-09-18 13:52:52 +0530

    added

--- Level-1/Recursion & Backtracking/Backtracking/N_queens.java
+++ Level-1/Recursion & Backtracking/Backtracking/N_queens.java
@@ -2,11 +2,59 @@ import java.util.Scanner;
 
 public class N_queens {
 
+    public static void printNQueens(int[][] chess, String qsf, int row) {
+
+        if (row == chess.length) {
+
+            System.out.println(qsf);
+            return;
+        }
+
+        for (int col = 0; col < chess[0].length; col++) {
+
+            if (isSafe(chess, row, col)) {
+                chess[row][col] = 1;
+                printNQueens(chess, qsf + row + "-" + col + ", ", row + 1);
+
+                chess[row][col] = 0;
+            }
+
+        }
+    }
+
+    public static boolean isSafe(int[][] chess, int row, int col) {
+
+        for (int i = row - 1, j = col; i >= 0; i--) {
+
+            if (chess[i][j] == 1)
+                return false;
+        }
+
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+
+            if (chess[i][j] == 1)
+                return false;
+        }
+
+        for (int i = row - 1, j = col + 1; i >= 0 && j < chess.length; i--, j++) {
+
+            if (chess[i][j] == 1)
+                return false;
+        }
+
+        return true;
+    }
+
     public static void main(String[] args) {
-        
+
         Scanner scn = new Scanner(System.in);
-     
+
+        int n = scn.nextInt();
+
+        int[][] chess = new int[n][n];
+
+        printNQueens(chess, "", 0);
         scn.close();
     }
-    
+
 }
