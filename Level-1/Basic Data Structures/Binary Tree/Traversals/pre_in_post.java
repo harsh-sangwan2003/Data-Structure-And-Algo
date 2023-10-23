8dcad19dd3332135ff5640c0694d329745f621ef
Harsh Sangwan <84121947+harsh-sangwan2003@users.noreply.github.com>
2023-10-23 16:42:28 +0530

    added

---
+++ Level-1/Basic Data Structures/Binary Tree/Traversals/pre_in_post.java
@@ -0,0 +1,125 @@
+import java.util.Stack;
+
+public class pre_in_post {
+
+    public static class Node {
+
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data, Node left, Node right) {
+
+            this.data = data;
+            this.left = left;
+            this.right = right;
+        }
+
+    }
+
+    public static class Pair {
+
+        Node node;
+        int state;
+
+        Pair(Node node, int state) {
+
+            this.node = node;
+            this.state = state;
+        }
+    }
+
+    public static void display(Node node) {
+
+        if (node == null)
+            return;
+
+        String str = "";
+
+        str += node.left == null ? "." : node.left.data + "";
+        str += "<-" + node.data + "->";
+        str += node.right == null ? "." : node.right.data + "";
+
+        System.out.println(str);
+
+        display(node.left);
+        display(node.right);
+    }
+
+    public static void traversal(Node node) {
+
+        if (node == null)
+            return;
+
+        System.out.println("Pre " + node.data);
+
+        traversal(node.left);
+        System.out.println("In " + node.data);
+
+        traversal(node.right);
+        System.out.println("Post " + node.data);
+    }
+
+    public static void main(String[] args) {
+
+        Integer[] arr = { 50, 25, 12, null, null, 37, 30, null, null, null, 75, 62, null, 70, null, null, 87, null,
+                null };
+
+        Stack<Pair> st = new Stack<>();
+        Node root = new Node(arr[0], null, null);
+
+        Pair rPair = new Pair(root, 1);
+        st.push(rPair);
+
+        int idx = 0;
+
+        while (st.size() != 0) {
+
+            Pair top = st.peek();
+
+            if (top.state == 1) {
+
+                idx++;
+                if (arr[idx] != null) {
+
+                    top.node.left = new Node(arr[idx], null, null);
+                    Pair lp = new Pair(top.node.left, 1);
+                    st.push(lp);
+                }
+
+                else {
+
+                    top.node.left = null;
+                }
+
+                top.state++;
+            }
+
+            else if (top.state == 2) {
+
+                idx++;
+                if (arr[idx] != null) {
+
+                    top.node.right = new Node(arr[idx], null, null);
+                    Pair rp = new Pair(top.node.right, 1);
+                    st.push(rp);
+                }
+
+                else {
+
+                    top.node.right = null;
+                }
+
+                top.state++;
+            }
+
+            else {
+
+                st.pop();
+            }
+        }
+
+        display(root);
+        traversal(root);
+    }
+}
---
+++ Level-1/Basic Data Structures/Binary Tree/node_to_root_path.java
@@ -0,0 +1,154 @@
+import java.io.*;
+import java.util.*;
+
+public class node_to_root_path {
+
+    public static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data, Node left, Node right) {
+            this.data = data;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public static class Pair {
+        Node node;
+        int state;
+
+        Pair(Node node, int state) {
+            this.node = node;
+            this.state = state;
+        }
+    }
+
+    public static Node construct(Integer[] arr) {
+        Node root = new Node(arr[0], null, null);
+        Pair rtp = new Pair(root, 1);
+
+        Stack<Pair> st = new Stack<>();
+        st.push(rtp);
+
+        int idx = 0;
+        while (st.size() > 0) {
+            Pair top = st.peek();
+            if (top.state == 1) {
+                idx++;
+                if (arr[idx] != null) {
+                    top.node.left = new Node(arr[idx], null, null);
+                    Pair lp = new Pair(top.node.left, 1);
+                    st.push(lp);
+                } else {
+                    top.node.left = null;
+                }
+
+                top.state++;
+            } else if (top.state == 2) {
+                idx++;
+                if (arr[idx] != null) {
+                    top.node.right = new Node(arr[idx], null, null);
+                    Pair rp = new Pair(top.node.right, 1);
+                    st.push(rp);
+                } else {
+                    top.node.right = null;
+                }
+
+                top.state++;
+            } else {
+                st.pop();
+            }
+        }
+
+        return root;
+    }
+
+    public static void display(Node node) {
+        if (node == null) {
+            return;
+        }
+
+        String str = "";
+        str += node.left == null ? "." : node.left.data + "";
+        str += " <- " + node.data + " -> ";
+        str += node.right == null ? "." : node.right.data + "";
+        System.out.println(str);
+
+        display(node.left);
+        display(node.right);
+    }
+
+    public static boolean find(Node node, int data) {
+        // write your code here
+        if (node == null)
+            return false;
+
+        if (node.data == data)
+            return true;
+
+        boolean left = find(node.left, data);
+        if (left)
+            return true;
+
+        boolean right = find(node.right, data);
+        if (right)
+            return true;
+
+        return false;
+    }
+
+    static ArrayList<Integer> res;
+
+    public static ArrayList<Integer> nodeToRootPath(Node node, int data) {
+        // write your code here
+        if (node == null)
+            return new ArrayList<>();
+
+        if (node.data == data) {
+            res.add(node.data);
+            return res;
+        }
+
+        boolean left = find(node.left, data);
+        if (left) {
+            res.add(node.data);
+            return res;
+        }
+
+        boolean right = find(node.right, data);
+        if (right) {
+            res.add(node.data);
+            return res;
+        }
+
+        return new ArrayList<>();
+        
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        Integer[] arr = new Integer[n];
+        String[] values = br.readLine().split(" ");
+        for (int i = 0; i < n; i++) {
+            if (values[i].equals("n") == false) {
+                arr[i] = Integer.parseInt(values[i]);
+            } else {
+                arr[i] = null;
+            }
+        }
+
+        int data = Integer.parseInt(br.readLine());
+
+        Node root = construct(arr);
+        boolean found = find(root, data);
+        System.out.println(found);
+
+        res = new ArrayList<>();
+        ArrayList<Integer> path = nodeToRootPath(root, data);
+        System.out.println(path);
+    }
+
+}
