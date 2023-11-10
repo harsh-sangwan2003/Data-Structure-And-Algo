6f00f8931139026c42248847656b5538d4476874
Harsh Sangwan <84121947+harsh-sangwan2003@users.noreply.github.com>
2023-11-10 20:14:19 +0530

    time-space-completed

---
+++ Level-1/Time & Space Complexity/pivot.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class pivot {
+
+    public static int findPivot(int[] arr) {
+        // write your code here
+        int lo = 0, hi = arr.length - 1;
+
+        while (lo < hi) {
+
+            int mid = lo + (hi - lo) / 2;
+
+            if (arr[mid] < arr[hi])
+                hi = mid;
+
+            else
+                lo = mid + 1;
+        }
+
+        return arr[hi];
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        Scanner scn = new Scanner(System.in);
+
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scn.nextInt();
+        }
+
+        int pivot = findPivot(arr);
+        System.out.println(pivot);
+
+        scn.close();
+
+    }
+
+}
---
+++ Level-1/Time & Space Complexity/target_sum_pair.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+public class target_sum_pair {
+
+    public static void targetSumPair(int[] arr, int target) {
+        // write your code here
+        int lo = 0, hi = arr.length - 1;
+
+        while (lo < hi) {
+
+            if (arr[lo] + arr[hi] == target) {
+
+                System.out.println(arr[lo] + ", " + arr[hi]);
+                lo++;
+                hi--;
+            }
+
+            else if (arr[lo] + arr[hi] < target)
+                lo++;
+
+            else
+                hi--;
+        }
+
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        Scanner scn = new Scanner(System.in);
+
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scn.nextInt();
+        }
+
+        int target = scn.nextInt();
+        targetSumPair(arr, target);
+
+        scn.close();
+    }
+
+}
